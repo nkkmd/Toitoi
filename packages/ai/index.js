6bbb869c4e5c220939244f28795e7aee298f77b8
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-19 21:26:04 +0900

    feat(ai): export v0.5.0 AI services

--- packages/ai/index.js
+++ packages/ai/index.js
@@ -4,9 +4,11 @@ module.exports = {
   ...require('./annotation'),
   ...require('./job_queue'),
   ...require('./fake_provider'),
+  ...require('./llama_cpp_provider'),
   ...require('./jsonl_store'),
   ...require('./worker'),
   ...require('./recovery'),
   ...require('./inspection_service'),
+  ...require('./review_service'),
   ...require('./promotion'),
 };
