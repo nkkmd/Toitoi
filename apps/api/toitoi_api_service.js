7703c5a9df2fc24a50b04ddf8d9ab2554c945431
Sawa <72678394+nkkmd@users.noreply.github.com>
2026-07-20 19:37:07 +0900

    feat(api): route v0.6.0 workflow mutations

--- apps/api/toitoi_api_service.js
+++ apps/api/toitoi_api_service.js
@@ -2,14 +2,25 @@
 
 const { createAiHttpService } = require('./ai_http_service');
 const { createStandardApiService } = require('./standard_api_service');
+const { createWorkflowHttpService } = require('./workflow_http_service');
 
 function createToitoiApiService(options = {}) {
   const standardService = options.standardService || createStandardApiService(options);
   const aiService = options.aiInspectionService
-    ? createAiHttpService({ inspectionService: options.aiInspectionService })
+    ? createAiHttpService({
+      inspectionService: options.aiInspectionService,
+      reviewService: options.aiReviewService || null,
+    })
+    : null;
+  const workflowService = options.workflowService
+    ? createWorkflowHttpService({ workflowService: options.workflowService })
     : null;
 
   function handleRequest(request = {}) {
+    if (workflowService) {
+      const workflowResult = workflowService.handleRequest(request);
+      if (workflowResult) return workflowResult;
+    }
     if (aiService) {
       const aiResult = aiService.handleRequest(request);
       if (aiResult) return aiResult;
@@ -21,6 +32,7 @@ function createToitoiApiService(options = {}) {
     handleRequest,
     standardService,
     aiService,
+    workflowService,
   });
 }
 
